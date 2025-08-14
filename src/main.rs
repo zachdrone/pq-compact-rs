67565ddbc95a933cafdf8d7a3b88391439d6d7bc
Zach Drone <zvdrone@gmail.com>
2025-08-14 08:33:05 -0400

    get all size info

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,42 @@ use std::fs::File;
 use std::os::unix::fs::MetadataExt;
 use tokio::fs;
 
-async fn get_file_sizes(directory: &str) -> Result<Vec<u64>, anyhow::Error> {
+#[derive(Debug)]
+struct FileInfo {
+    path: String,
+    file_size: u64,
+    avg_row_group_size: i64,
+}
+
+impl FileInfo {
+    fn is_candidate(&self) -> bool {
+        if self.file_size < 64 * 1024 || self.avg_row_group_size < 32 * 1024 {
+            return true;
+        }
+        return false;
+    }
+}
+
+async fn get_file_sizes(directory: &str) -> Result<Vec<FileInfo>, anyhow::Error> {
     let mut handles = Vec::new();
     let mut results = Vec::new();
     for entry in glob(&format!("{}/*.parquet", directory)).expect("Failed to read glob pattern") {
         if let Ok(fp) = entry {
             handles.push(tokio::spawn(async move {
-                let f = tokio::fs::metadata(fp).await.unwrap();
-                f.size()
+                let file_size = tokio::fs::metadata(&fp).await.unwrap().size();
+                let file = File::open(&fp).unwrap();
+                let md = ParquetMetaDataReader::new()
+                    .with_page_indexes(true)
+                    .parse_and_finish(&file)
+                    .unwrap();
+                let total_size: i64 = md.row_groups().iter().map(|rg| rg.total_byte_size()).sum();
+                let row_group_count = md.num_row_groups();
+
+                FileInfo {
+                    path: fp.to_str().unwrap().to_string(),
+                    file_size: file_size,
+                    avg_row_group_size: total_size / (row_group_count as i64),
+                }
             }))
         }
     }
@@ -22,9 +50,6 @@ async fn get_file_sizes(directory: &str) -> Result<Vec<u64>, anyhow::Error> {
         results.push(handle.await.unwrap());
     }
 
-    // for result in results {
-    //     println!("{:?}", result);
-    // }
     Ok(results)
 }
 
@@ -35,30 +60,4 @@ async fn main() {
     for result in results {
         println!("{:?}", result);
     }
-
-    // let f = fs::metadata("files/file_01.parquet")
-    //     .await
-    //     .expect("Failed to read metadata");
-    // let size = f.size();
-    //
-    //
-    // print!("{:?}", size.size());
-
-    // let file = File::open("file_01.parquet").unwrap();
-    // let md = ParquetMetaDataReader::new()
-    //     .with_page_indexes(true)
-    //     .parse_and_finish(&file)
-    //     .unwrap();
-    // // println!("{}", md.row_groups()[0].total_byte_size());
-    // // println!("{:?}", md.row_groups()[0]);
-    // println!("{:?}", md.file_metadata());
-    // let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
-    // println!("Converted arrow schema is: {}", builder.schema());
-    //
-    // let mut reader = builder.build().unwrap();
-    //
-    //
-    // let record_batch = reader.next().unwrap().unwrap();
-    //
-    // println!("Read {} records.", record_batch.num_rows())
 }
