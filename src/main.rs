67f9fffb09b4955a3ef22e170bf0a7243884725c
Zach Drone <zvdrone@gmail.com>
2025-08-14 07:52:17 -0400

    functionify

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,43 @@ use std::fs::File;
 use std::os::unix::fs::MetadataExt;
 use tokio::fs;
 
-#[tokio::main]
-async fn main() {
-    let f = fs::metadata("files/file_01.parquet")
-        .await
-        .expect("Failed to read metadata");
-    let size = f.size();
-    // print!("{:?}", size.size());
-
-    let mut futures = Vec::new();
+async fn get_file_sizes(directory: &str) -> Result<Vec<u64>, anyhow::Error> {
+    let mut handles = Vec::new();
     let mut results = Vec::new();
-    for entry in glob("files/*.parquet").expect("Failed to read glob pattern") {
+    for entry in glob(&format!("{}/*.parquet", directory)).expect("Failed to read glob pattern") {
         if let Ok(fp) = entry {
-            futures.push(tokio::spawn(async move {
+            handles.push(tokio::spawn(async move {
                 let f = tokio::fs::metadata(fp).await.unwrap();
                 f.size()
             }))
         }
     }
 
-    for handle in futures {
+    for handle in handles {
         results.push(handle.await.unwrap());
     }
 
+    // for result in results {
+    //     println!("{:?}", result);
+    // }
+    Ok(results)
+}
+
+#[tokio::main]
+async fn main() {
+    let results = get_file_sizes("files").await.unwrap();
+
     for result in results {
-        println!("{}", result);
+        println!("{:?}", result);
     }
+
+    // let f = fs::metadata("files/file_01.parquet")
+    //     .await
+    //     .expect("Failed to read metadata");
+    // let size = f.size();
+    //
+    //
+    // print!("{:?}", size.size());
 
     // let file = File::open("file_01.parquet").unwrap();
     // let md = ParquetMetaDataReader::new()
