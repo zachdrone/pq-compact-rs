2e1d9db3baf6f9d155f5f482173ed1d5147801db
Zach Drone <zvdrone@gmail.com>
2025-08-18 08:38:10 -0400

    add arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use crate::plan::get_compaction_candidates;
+use clap::Parser;
 
 pub mod plan;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    dir: String,
+}
+
 fn main() {
-    let results = get_compaction_candidates("files").unwrap();
+    let args = Args::parse();
+
+    let results = get_compaction_candidates(&args.dir).unwrap();
 
     for result in results {
         println!("{:?}", result);
